55c495f4c99d756617ed90d78fd5f842568cd91f
AndyMelm <recast321@gmail.com>
2023-05-20 17:18:45 +0300

    first commit

---
+++ Back/base_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
+from . import views
+
+urlpatterns = [
+    path("login/", TokenObtainPairView.as_view()),
+    path("about/", views.about),
+    path("contact/", views.contact),
+    path('register/', views.register),
+    path('product/<pk>', views.ProductView.as_view()),
+    path('product/', views.ProductView.as_view()),
+
+]
